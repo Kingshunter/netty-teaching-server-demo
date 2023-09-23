1a3a0f048d020431793618ae4d7f98ba7cecaccb
hunter <babysai1988@vip.qq.com>
2023-09-24 01:21:09 +0800

    initial code

---
+++ src/main/java/com/hunter/teaching/netty/demo/server/chapter01/NettyTimeServerDemo1.java
@@ -0,0 +1,49 @@
+package com.hunter.teaching.netty.demo.server.chapter01;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * This Demo is not consider the tcp stick package or tcp unpacking
+ */
+public class NettyTimeServerDemo1 {
+
+    public void bind(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new NettyChildChannelHandlerClientDemo1());
+            ChannelFuture f = b.bind(port).sync();
+
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    private class NettyChildChannelHandlerClientDemo1 extends ChannelInitializer<SocketChannel> {
+
+        @Override
+        protected void initChannel(SocketChannel sc) throws Exception {
+            sc.pipeline().addLast(new NettyTimeServerHandlerDemo1());
+
+        }
+
+    }
+    
+    public static void main(String[] args) throws Exception {
+        NettyTimeServerDemo1 nettyTimeServerDemo1 = new NettyTimeServerDemo1();
+        nettyTimeServerDemo1.bind(10080);
+    }
+
+}
